58e99a4d5f9bf58bada3d2812ca4b6794fbdbe08
rumz <rumzster@gmail.com>
2010-01-18 12:02:36 -0500

    Initial commit.

---
+++ countries.py
@@ -0,0 +1,23 @@
+"""
+TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+    Open file with a list of countries.   >> D:\PPython\countrylist.txt
+    Put the list of countries into a country list/tuple.
+    Open another file with past results.
+    Randomly get one country from the country list.
+    Store (country + date) into results list.
+    Store results back into file.
+TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+"""
+import os
+
+countrylist
+filename = "D:\PPython\countrylist.txt"
+
+f = open(filename, mode='r', bufsize=-1)
+while not f.eof:
+
+
+
+i = 0;
+#for i in 1..30 :
+print i
